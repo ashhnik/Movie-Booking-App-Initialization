52b16077fdb87b0b8c1c5273446af9bcf581ba93
Aditya Singh <singh.aditya1394@gmail.com>
2022-08-18 22:24:12 +0530

    Checkpoint 5

--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactDOM from 'react-dom';
 import Header from "../../common/header/Header.js";
 import "./Home.css";
 import { withStyles } from '@material-ui/core/styles';
@@ -22,6 +23,7 @@ import Button from '@material-ui/core/Button';
 import genre from "../../common/genre";
 import artists from "../../common/artists";
 import genres from '../../common/genre';
+import Details from '../details/Details.js';
 
 
 const styles = theme => ({
@@ -93,17 +95,16 @@ releaseDateEndHandler = event => {
 }
 
 movieClickHandler = (movieId) => {
-  this.props.history.push('/movie/' + movieId);
+  ReactDOM.render(<Details movieId={movieId} />, document.getElementById('root'));
 }
 
 
   render() {
     const { classes } = this.props;
-    console.log("Ayush" + this.state.movieName);
     var filterMovie=moviesData.filter((movie)=>{
     return(movie.title=== this.state.movieName ||this.state.artists.includes( (movie.artists[0].first_name+" "+movie.artists[0].last_name)))
   })
-    if(this.state.movieName.length ==0  && this.state.artists.length == 0){
+    if(this.state.movieName.length ===0  && this.state.artists.length === 0){
       filterMovie=moviesData;
     }
     
@@ -132,7 +133,7 @@ movieClickHandler = (movieId) => {
             <div className="left">
             <GridList cellHeight={350} cols={4} className={classes.gridListMain}>
               {filterMovie.map((movie) => (
-                <GridListTile className="released-movie-grid-item"
+                <GridListTile  onClick={() => this.movieClickHandler(movie.id)} className="released-movie-grid-item"
                   key={"grid" + movie.id}
                 >
                   <img
@@ -172,7 +173,6 @@ movieClickHandler = (movieId) => {
                                     <InputLabel htmlFor="select-multiple-checkbox">Genres</InputLabel>
                                     <Select
                                         multiple
-                                        input={<Input id="select-multiple-checkbox-genre" />}
                                         input={<Input id="select-multiple-checkbox-genre" />}
                                         renderValue={selected => selected.join(',')}
                                         value={this.state.genres}
